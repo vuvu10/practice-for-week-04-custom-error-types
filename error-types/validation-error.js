fc47f78a950316ff4f94f1de0194e26383ea04ee
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-23 13:36:54 -0400

    my commits

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,14 @@
 // Your code here
 
+class ValidationError extends Error {
+  constructor(message = 'Invalid input') {
+    super(message);
+    this.name = 'ValidationError';
+
+  }
+
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
