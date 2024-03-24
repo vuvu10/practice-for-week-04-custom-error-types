7edf6cca4bafe76c5065b5f65f2e74cf8e2faa83
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-24 13:11:29 -0400

    another commit

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -2,6 +2,17 @@ const ValidationError = require('./validation-error');
 
 // Your code here
 
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength = null) {
+   super(
+    excessLength
+     ? `Maximum length exceeded by ${excessLength}`
+     : "Maximum length exceeded"
+   );
+   this.name = "MaximumLengthExceededError";
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
